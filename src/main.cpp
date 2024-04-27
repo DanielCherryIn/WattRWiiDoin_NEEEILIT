babadfd5dad2c6963cfceb025ccf6a17e98b2c61
Daniel Cherry InAas <up201907258@fe.up.pt>
2024-04-27 15:37:25 +0100

    changed config a bit - also added LED blink :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  digitalWrite(LED_BUILTIN, HIGH);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(1000);
 }
